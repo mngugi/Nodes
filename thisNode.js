aef6e937f7adaade7f65a11cbbf49d38ed4be694
M.Ngugi <mngugi7@gmail.com>
2024-11-10 12:50:50 +0300

    Update thisNode.js

--- thisNode.js
+++ thisNode.js
@@ -1,14 +1,13 @@
 // thisNode.js
 
 const http = require('http');
-var dt = require('./DateTimeModule.js')
+const dt = require('./DateTimeModule.js')
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
+  res.write("The date and time are currently: " + dt.DateTimeModule());
   res.end('Global Warming!\n');
 });
-
-
 
 const PORT = 3000;
 server.listen(PORT, () => {
