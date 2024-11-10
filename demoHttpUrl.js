47cc8af30e5933477e415db324534be9aa27e007
M.Ngugi <mngugi7@gmail.com>
2024-11-10 20:13:52 +0300

    Update demoHttpUrl.js

--- demoHttpUrl.js
+++ demoHttpUrl.js
@@ -1,6 +1,9 @@
 var http = require('http');
+
 http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/html'});
-  res.write(req.url);
+  var q = url.parse(req.url, true).query;
+  
+  var txt = q.year + " " + q.month;
   res.end();
 }).listen(8080);
