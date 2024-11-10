4c81b9b342f18692bfc0f2fde69b36f7d14697c5
M.Ngugi <mngugi7@gmail.com>
2024-11-10 21:15:06 +0300

    Update readFil.js

--- readFil.js
+++ readFil.js
@@ -2,7 +2,7 @@ var http = require('http');
 var fs = require('fs');
 http.createServer(function (req, res) {
   //Open a file on the server and return its content:
-  fs.readFile('demofile1.html', function(err, data) {
+  fs.readFile('page34_2.html', function(err, data) {
     res.writeHead(200, {'Content-Type': 'text/html'});
     res.write(data);
     return res.end();
