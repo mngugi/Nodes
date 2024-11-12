217ae17815ecc1a90ea2242f4d49b7ed7e7346dd
M.Ngugi <mngugi7@gmail.com>
2024-11-12 12:46:25 +0300

    appendFile.js

--- appendFile.js
+++ appendFile.js
@@ -1,6 +1,6 @@
 var fs = require('fs');
 
-fs.appendFile('mynewfile1.txt', 'Hello content!', function (err) {
+fs.appendFile('mynewfile1.txt', 'Hello content! That is Goodnews', function (err) {
   if (err) throw err;
   console.log('Saved!');
 });
